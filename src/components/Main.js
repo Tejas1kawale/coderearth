13aeb39c2423a0329eab9e9acd91cb00e8974a92
Tejas Kawale <tejaskawale5@gmail.com>
2023-12-28 00:29:29 +0530

    Navbar create and small intro added

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Main() {
+  return (
+    <>
+        <div className="container my-4">
+            Hello My name is Tejas.
+
+            I am a software developer doing specialization in React-js. 
+
+
+        </div>
+    </>
+  )
+}
